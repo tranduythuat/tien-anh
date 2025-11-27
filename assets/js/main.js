1bac6c1c814babfa5b08dbb6aad4448732e550e4
tranduythuat <thuattran1810@gmail.com>
2025-11-27 10:08:01 +0700

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,5 +52,92 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
   const btn = document.getElementById('player-btn');
+  const labelMusic = document.getElementById('music-label');
   btn.addEventListener('click', toggleMusic);
+  labelMusic.addEventListener('click', toggleMusic);
+
+  const formNhaTrai = document.forms["rsvpForm-nhatrai"];
+  const formNhaGai = document.forms["rsvpForm-nhagai"];
+  if (formNhaTrai) {
+    formNhaTrai.addEventListener("submit", (e) => handleFormSubmit(e, 'nhatrai'));
+  }
+
+  if (formNhaGai) {
+    formNhaGai.addEventListener("submit", (e) => handleFormSubmit(e, 'nhagai'));
+  }
 });
+
+async function handleFormSubmit(e, code) {
+  e.preventDefault();
+
+  const form = e.target;
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  console.log("🚀 ~ handleFormSubmit ~ data:", data);
+
+  const {
+    name: name,
+    attendance: attendance,
+    dietary: dietary,
+    message: message,
+  } = data;
+  console.log("🚀 ~ handleFormSubmit 2~ data:", data);
+
+  // Thông báo khi bắt đầu gửi
+  Swal.fire({
+    title: 'Đang gửi ...',
+    text: "Vui lòng chờ trong giây lát",
+    icon: "info",
+    allowOutsideClick: false,
+    didOpen: () => {
+      Swal.showLoading();
+    },
+  });
+
+  const urlObj = {
+    nhatrai: "https://script.google.com/macros/s/AKfycbwkg-nttei7IeHRhZfb878dmted-EaIn8sNX6t-XjWbn0vpuPxgN2eIHvI9h6oDngj4/exec?sheet=nha-trai",
+    nhagai: "https://script.google.com/macros/s/AKfycbwkg-nttei7IeHRhZfb878dmted-EaIn8sNX6t-XjWbn0vpuPxgN2eIHvI9h6oDngj4/exec?sheet=nha-gai"
+  }
+
+  
+  const url = urlObj[code]??"";
+  console.log('code', url);
+  
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        name,
+        attendance,
+        dietary,
+        message
+      }),
+    });
+
+    const result = await res.json().catch(() => ({}));
+    console.log("Server response:", result);
+
+    form.reset();
+
+    // Thông báo thành công
+    Swal.fire({
+      title: "Thành công!",
+      text: "Cảm ơn bạn đã gửi phản hồi, thông tin đã được gửi đến dâu rể rồi nha",
+      icon: "success",
+      confirmButtonText: "OK",
+      confirmButtonColor: "#000",
+    });
+  } catch (error) {
+    console.error("Error:", error);
+
+    // Thông báo lỗi
+    Swal.fire({
+      title: "Lỗi!",
+      text: "OPPS! Đã xảy ra lỗi: " + error.message,
+      icon: "error",
+      confirmButtonText: "Thử lại",
+      confirmButtonColor: "#000",
+    });
+  }
+}
